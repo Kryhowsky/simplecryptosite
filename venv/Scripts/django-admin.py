9042c7fcd2e8b697c94ccbe347c2ca8bd73d61fa
P Kry <piter71-1997@go2.pl>
2020-01-17 00:00:59 +0100

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\piotr.kryszkiewicz\Desktop\cryptocurrency\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
